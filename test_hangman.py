030796708b59564898172de7385d2831b2917075
Noufal Ibrahim <noufal@nibrahim.net.in>
2023-05-26 12:25:05 +0530

    GREEN : Adds test for lowercase

--- test_hangman.py
+++ test_hangman.py
@@ -8,6 +8,10 @@ def test_select_random_word_no_non_alpha_chars():
     secret_word = hangman.get_random_word()
     assert secret_word.isalpha()
 
+def test_select_random_word_no_capitals():
+    secret_word = hangman.get_random_word()
+    assert secret_word.islower()
+
 
 
 
