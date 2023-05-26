1a2d1fdf28835a69ce447ee1a37631f59974e4fd
Noufal Ibrahim <noufal@nibrahim.net.in>
2023-05-26 12:36:20 +0530

    REFACTOR : All tests pass

--- hangman.py
+++ hangman.py
@@ -1,2 +1,12 @@
+import random
+
+
 def get_random_word():
-    return "elephant"
+    candidate_words = []
+    with open("/usr/share/dict/words") as f:
+        for word in f:
+            word = word.strip()
+            if len(word) >= 6 and word.islower() and word.isalpha():
+                candidate_words.append(word)
+    word = random.choice(candidate_words)
+    return word
