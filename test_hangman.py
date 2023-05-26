3a1fd7293dae324127dc9a28d824cc3e6e2970c1
Noufal Ibrahim <noufal@nibrahim.net.in>
2023-05-26 12:24:22 +0530

    GREEN : Adds test for alphabetic

--- test_hangman.py
+++ test_hangman.py
@@ -4,6 +4,10 @@ def test_select_random_word_min_length():
     secret_word = hangman.get_random_word()
     assert len(secret_word) >= 6
 
+def test_select_random_word_no_non_alpha_chars():
+    secret_word = hangman.get_random_word()
+    assert secret_word.isalpha()
+
 
 
 
